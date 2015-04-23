fb251df999d213380269a580aa7763abcbdacfb9
Alex Russell <amrussell@smu.edu>
2015-04-22 19:48:28 -0500

    added css

--- UI Mockups/userpage.php
+++ UI Mockups/userpage.php
@@ -16,6 +16,7 @@
 	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/styles.css" />
+	<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/ui-lightness/jquery-ui.css" />
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
 

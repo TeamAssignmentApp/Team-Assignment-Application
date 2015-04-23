e981ee73814fc977efa60af5a50d1aba966bbe8d
Alex Russell <amrussell@smu.edu>
2015-04-22 20:52:17 -0500

    preliminary viewprojects functionality

--- UI Mockups/viewprojects.php
+++ UI Mockups/viewprojects.php
@@ -8,12 +8,36 @@
 <!doctype html>
 <html>
 	<head>
+		<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/styles.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 		<style>
 			
 		</style>
+		<script>
+			$(document).ready(function(){
+				$.get("api/class.php", {id:1, token:'9164fe76dd046345905767c3bc2ef54'}, function(data){
+					var projects = JSON.parse(data["projects"]);
+					$(projects).each(function(index,value){
+						$("#projectContainer").append("" +
+							'<div class="col-md-4">' +
+								'<div class="well">' +
+									'<h2>' + value.projectName + '</h2>' +
+									'<h3>Description</h3>' +
+									'<p>' + value.projectDesc '</p>' +
+									'<h3>Required Majors</h3>' +
+									'<ul class="list-group">');
+							$(value.majors).each(function(index,value){
+								$("#projectContainer").append('<li class="list-group-item">' + value.number + ' ' + value.name + '</li>');
+							});
+							$("#projectContainer").append('</ul></div></div>');
+
+									
+					});
+				});
+			})
+		</script>
 	</head>
 	<body>
 		<div class="container">
@@ -21,114 +45,10 @@
                 <h1>SMU Lyle Multidisciplinary Senior Design</h1>
             </div>
 		</div>
-		<div class="container" style="margin-top:50px">
+		<div class="container" id="projectContainer" style="margin-top:50px">
 			<div class="col-md-4">
 				<div class="well">
 					<h2>Project 1</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 2</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 3</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 4</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 5</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 6</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 7</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 8</h2>
-					<h3>Description</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
-					<h3>Required Majors</h3>
-					<ul class="list-group">
-						<li class="list-group-item">2 Computer Science</li>
-						<li class="list-group-item">2 Mechanical Engineering</li>
-						<li class="list-group-item">2 Electrical Engineering</li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md-4">
-				<div class="well">
-					<h2>Project 9</h2>
 					<h3>Description</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor non lacus congue lacinia. Nunc nulla ipsum, laoreet in augue id, pharetra hendrerit eros. Pellentesque dignissim luctus lacus at condimentum. Aenean vitae diam nec tellus pellentesque egestas. Aenean a aliquam lorem. Praesent accumsan finibus nisl, sed vestibulum nibh tristique in. Vivamus semper sed justo quis varius. Donec consectetur neque egestas risus ultricies vestibulum. Etiam finibus mi ac dui imperdiet, ut pharetra urna hendrerit. Duis sagittis nisl eu purus accumsan venenatis. Morbi ex diam, efficitur id egestas sit amet, euismod et purus.</p>
 					<h3>Required Majors</h3>

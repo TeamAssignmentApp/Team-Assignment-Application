88e6c8f58b56cf9698f910380670a14fc4ae7eff
Alex Russell <amrussell@smu.edu>
2015-04-27 21:52:01 -0500

    debugging

--- UI Mockups/AdminDash.php
+++ UI Mockups/AdminDash.php
@@ -52,11 +52,8 @@
 								classTable.row.add([parsedClassData["name"], parsedClassData["startTime"], parsedClassData["endTime"], "a"]).draw();
 							}	
 							$.get("api/user.php", {id: user["id"], token:'9164fe76dd046345905767c3bc2ef54'}, function(userData){
-								console.log("userData");
-								console.log(userData);
-								console.log("fname");
-								console.log(userData["fname"]);
-								userTable.row.add([userData["fname"] + " " + userData["lname"], userData["major"], userData["email"]]).draw();
+								var parsedUserData = JSON.parse(userData);
+								userTable.row.add([parsedUserData["fname"] + " " + parsedUserData["lname"], parsedUserData["major"], parsedUserData["email"]]).draw();
 							});
 						});
 						$(thisClassProjects).each(function(index,proj){

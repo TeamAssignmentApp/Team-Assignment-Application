eb49f511065dcc40a003bbe37d179c2182803f18
CaptainMustang8 <nmorris@smu.edu>
2015-04-29 23:58:20 -0500

    Moved the button

--- UI Mockups/userpage.php
+++ UI Mockups/userpage.php
@@ -85,8 +85,8 @@
 			</div>
 			<div class="col-md-6">
 				<div class="well" style="padding-top:0px;" id="projReqs">
-					<h4>Prioritize your project requests.</h4>
-					<a class="btn btn-primary" id="projectButton" onclick="window.open('http://ec2-52-11-229-124.us-west-2.compute.amazonaws.com/viewprojects.php')">List of Projects</a>
+					<h4 style="display:inline-block">Prioritize your project requests.</h4>
+					<a class="btn btn-primary" id="projectButton" onclick="window.open('http://ec2-52-11-229-124.us-west-2.compute.amazonaws.com/viewprojects.php')" style="display:inline-block">List of Projects</a>
 				</div>
 			</div>
 			<div class="col-md-6">

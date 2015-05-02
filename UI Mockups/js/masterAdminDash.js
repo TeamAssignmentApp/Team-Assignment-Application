658fa99cc07033f030633a6aa3dcdc57c09927d1
Alex Russell <amrussell@smu.edu>
2015-05-02 16:46:25 -0500

    working on maste admin dash

--- UI Mockups/js/masterAdminDash.js
+++ UI Mockups/js/masterAdminDash.js
@@ -25,8 +25,6 @@ $(document).ready(function(){
 		});
 		$(classes).each(function(index,classID){
 			$.get("api/class.php", {id: classID, token:'9164fe76dd046345905767c3bc2ef54'}, function(classData){
-				console.log('classData');
-				console.log(classData);
 				var parsedClassData = JSON.parse(classData);
 				console.log("parsedClassData");
 				console.log(parsedClassData);

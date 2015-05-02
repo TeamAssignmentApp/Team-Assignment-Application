d7bcbd89b67f07245bf3bcb250a2b5ea6f99dc44
CaptainMustang8 <nmorris@smu.edu>
2015-05-02 12:39:27 -0500

    Experimenting with logout button display location

--- UI Mockups/userpage.php
+++ UI Mockups/userpage.php
@@ -102,9 +102,10 @@
 		<div class="container">
 			<div class="col-md-12" id="header" style="text-align:center">
 				<img src="css/LyleLogo.png" alt="LyleLogo" height="100" width="800">
+				<a href="logout.php" class="btn btn-danger" style="display:inline-block">Logout</a>
 			</div>
 
-			<a href="logout.php" class="btn btn-danger" style="display:inline-block">Logout</a>
+			<!-- <a href="logout.php" class="btn btn-danger" style="display:inline-block">Logout</a> -->
 
 			<div class="col-md-6">
 				<div class="well" style="padding-top:0px;" id="projReqs">

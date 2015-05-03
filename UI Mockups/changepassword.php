188c42c55d81e9446382378bb7d37a9200695579
CaptainMustang8 <nmorris@smu.edu>
2015-05-03 01:05:44 -0500

    Working with changepassword

--- UI Mockups/changepassword.php
+++ UI Mockups/changepassword.php
@@ -1,8 +1,8 @@
 <?php
-	session_start();
-	if(!isset($_SESSION['login_user'])){
-		header("location: login.php");
-	}
+	// session_start();
+	// if(!isset($_SESSION['login_user'])){
+	// 	header("location: login.php");
+	// }
 ?>
 
 

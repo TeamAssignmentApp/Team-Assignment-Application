487043db2072e2473e7d7988c420082a7bc6e487
Alex Russell <amrussell@smu.edu>
2015-05-03 03:12:36 -0500

    messing with admindashes

--- UI Mockups/js/masterAdminDash.js
+++ UI Mockups/js/masterAdminDash.js
@@ -26,8 +26,8 @@ $(document).ready(function(){
 		$(classes).each(function(index,classID){
 			$.get("api/class.php", {id: classID, token:'9164fe76dd046345905767c3bc2ef54'}, function(classData){
 				var parsedClassData = JSON.parse(classData);
-				//console.log("parsedClassData");
-				//console.log(parsedClassData);
+				console.log("parsedClassData");
+				console.log(parsedClassData);
 				var allUsersAllProjects = parsedClassData["users"];
 				var thisClassProjects = parsedClassData["projects"];
 				//console.log('allUsersAllProjects');

502c546b19cfcc8206f2f5a648cb3d7386198ba5
TeamAssignment <lyleteamassignment@gmail.com>
2015-05-04 10:39:05 +0000

    server

---
+++ UI Mockups/changepasswordUI.php
@@ -0,0 +1,73 @@
+<?php
+	// session_start();
+	// if(!isset($_SESSION['login_user'])){
+	// 	header("location: login.php");
+	// }
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+        <script>
+        	$(document).ready(function(){
+        		$.ajax({
+					url: 'api/user.php', 
+					type: 'PUT',
+					data: {
+						token: '9164fe76dd046345905767c3bc2ef54',
+						id: idToEdit,
+						name: editProjectName,
+						descrip: editProjectDescription,
+						file: editProjectFileLink,
+						classId: editProjectClassSelect
+					}, 
+					success: function(){
+						location.reload();
+					}
+				});
+        	});
+        </script>
+	</head>
+	<body>
+		<div class="container">
+            <div class="col-md-12" id="header" style="text-align:center">
+				<img src="css/LyleLogo.png" alt="LyleLogo" height="100" width="800">
+				<a href="logout.php" class="btn btn-danger" style="display:inline-block; float:right; margin-top:20px">Logout</a>
+			</div>
+		</div>
+		<div class="container" style="margin-top:150px">
+			<div class="col-md-3"></div>
+			<form action = "loginCheck.php" method="post">
+				<div class="col-md-6 well">
+					<h3 style="margin-top:0px">Change your password.</h3>
+					<div class="input-group" style="margin-bottom:10px">
+						<span class="input-group-addon">
+							Email
+						</span>
+						<input class="form-control" type="text" name = "email"/>
+					</div>
+					<div class="input-group" style="margin-bottom:10px">
+						<span class="input-group-addon">
+							New Password
+						</span>
+						<input class="form-control" type="password" id="password"/>
+					</div>
+					<div class="input-group" style="margin-bottom:10px">
+						<span class="input-group-addon">
+							Confirm Password
+						</span>
+						<input class="form-control" type="password" id="confirmPassword"/>
+					</div>
+					<a href="logout.php" class="btn btn-primary btn-sm" style="margin-bottom:10px" name="submit" type="submit">Submit New Password</a>
+				</div>
+			</form>
+			<div class="col-md-3"></div>
+		</div>
+
+	</body>
+</html>

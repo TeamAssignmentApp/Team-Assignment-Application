b6bff62965629f2f7c06ebaa207eb66b8e8572fd
Alex Russell <amrussell@smu.edu>
2015-05-06 17:38:15 -0500

    project skills

--- UI Mockups/js/masterAdminDash.js
+++ UI Mockups/js/masterAdminDash.js
@@ -198,10 +198,10 @@ $(document).ready(function(){
 							console.log('adding skill to project');
 							console.log(skill);
 							$("#projSkills-" + proj['id']).append(skill['skillName'] + ', ');
-							setTimeout(function(){
+							/*setTimeout(function(){
 								var skillsFromTable = $("#projSkills-" + proj['id']).text();
 								$("#projSkills-" + proj['id']).text(skillsFromTable.substring(0, skillsFromTable.length - 2));		
-							}, 3000);
+							}, 3000);*/
 						});
 					});
 				});
